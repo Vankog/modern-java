38bf3426c5329577ec804ea924710193a157c19d
Vankog <m.vankog@gmail.com>
2023-12-11 13:47:57 +0100

    apply jacoco and optimizations

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
-    id("java")
+    java
+    `test-report-aggregation`
+    `jacoco-report-aggregation`
 }
 
 group = "org.example"
@@ -10,24 +12,43 @@ repositories {
     mavenCentral()
 }
 
+tasks.withType(Test::class).configureEach({
+    finalizedBy(
+        tasks.testAggregateTestReport,
+        tasks.testCodeCoverageReport
+    )
+})
+
+
+private fun optimalForkCount(): Int {
+    return (Runtime.getRuntime().availableProcessors() / 2).coerceAtLeast(1)
+}
+private val optimalForkCount = optimalForkCount()
 
 subprojects {
 
     apply(plugin="java")
+    apply(plugin="jacoco")
 
     java({
         toolchain {
             languageVersion.set(JavaLanguageVersion.of(21))
         }
     })
+    
+    jacoco({
+        toolVersion = "0.8.11" // https://github.com/jacoco/jacoco/releases
+    })
 
     tasks.withType<JavaCompile> {
         val compilerArgs = options.compilerArgs
         compilerArgs.add("--enable-preview")
+        options.encoding = "UTF-8"
     }
 
     tasks.test {
         useJUnitPlatform()
+        setMaxParallelForks(optimalForkCount)
         jvmArgs(listOf("--enable-preview"))
     }
 }
